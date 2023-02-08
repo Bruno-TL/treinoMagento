fcea235350388a0d1d81443357a178c47020fb85
Thiago <tbgt35@hotmail.com>
2023-02-08 16:56:46 -0300

    Terminando o estudo sobre Arguments types

--- src/app/code/Interfaces/InterfaceModule/Controller/Page/Index.php
+++ src/app/code/Interfaces/InterfaceModule/Controller/Page/Index.php
@@ -6,21 +6,25 @@ use Magento\Framework\App\Action\Context;
 use Magento\Framework\App\Action\Action;
 use Interfaces\InterfaceModule\Api\NameInterface;
 use Interfaces\InterfaceModule\Model\Product\Shoe;
+use Interfaces\IntefaceModule\Model\Product\Shirt;
 
 class Index extends Action
 {
     protected $name;
     protected $product;
+    protected $shirt;
 
-    public function __construct(Context $context, NameInterface $name, Shoe $product)
+    public function __construct(Context $context, NameInterface $name, Shoe $product, Shirt $shirt)
     {
         parent::__construct ($context);
         $this->name =$name;
         $this->product = $product;
+        $this->shirt = $shirt;
+
     }
 
     public function execute()
     {
-        echo $this->product->getShoe() . " " . $this->name->getName() ;
+        echo $this->product->getShoe() . " " . $this->name->getName() . " " . $this->shirt;
     }
 }
